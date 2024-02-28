e19a2bef7ce89f7c763479a047e3b462010912bc
Hassaan Ahmed <hassaanahmedc@gmail.com>
2024-02-28 15:22:58 +0500

    added logo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
+import.meta.glob([
+    "../images/**",
+  ]);
 import "./bootstrap";
 import Alpine from "alpinejs";
 window.Alpine = Alpine;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,49 @@
         content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
+    {{-- <link rel="stylesheet"
+        href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"
+        defer></script> --}}
     <title>
         {{ config('app.name') }}
     </title>
 </head>
 
-<body x-data="{ open: false }">
-    <h1 class="text-3xl font-bold underline">
-        {{ config('app.name') }}
-    </h1>
+<body x-data="{ open: false }"
+    class="">
+    <header class="">
+        <nav class="w-full p-4 flex items-center justify-between bg-gray-700">
+            <div class="flex items-center">
+                <img src="{{ Vite::asset('/resources/svgs/logo.svg') }}"
+                    class=" w-16 mr-2"
+                    alt="Logo">
+
+                <h1 class="text-lg font-bold">
+                    {{ config('app.name') }}
+                </h1>
+            </div>
+            <ul class="flex items-center">
+                <li class="ml-6 hidden">Home</li>
+                <li class="ml-6 hidden">About Us</li>
+                <li class="ml-6 hidden">Services</li>
+                <li class="ml-6 hidden">Contact Us</li>
+                <li class="ml-6 hidden">Testimonials</li>
+                <li class="ml-6 hidden">Book an Appointment</li>
+            </ul>
+        </nav>
+
+
+    </header>
+    <section id=heroImg>
+        <div>
+            <figure>
+                <img src=""
+                    alt="">
+            </figure>
+        </div>
+    </section>
+
 </body>
 
 </html>
